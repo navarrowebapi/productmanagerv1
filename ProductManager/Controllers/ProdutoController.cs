ea37acf3dd1d247c54fbae14d480243a0ef0b8a3
navarrowebapi <navarro.fabio@gmail.com>
2018-02-22 19:44:27 -0300

    commit inicial

---
+++ ProductManager/Controllers/ProdutoController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductManager.Models;
+
+namespace ProductManager.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        static List<Produto> listaDeProdutos = new List<Produto>
+        {
+            new Produto { Id = 1, Nome = "Tomato Soup", Categoria = "Groceries",    Preco = 1 },
+            new Produto { Id = 2, Nome = "Yo-yo",       Categoria = "Toys",         Preco = 3.75M },
+            new Produto { Id = 3, Nome = "Hammer",      Categoria = "Hardware",     Preco = 16.99M }
+        };
+        // GET: Produto
+        public ActionResult Index()
+        {
+            return View(listaDeProdutos);
+        }
+
+        // GET: Produto/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Produto/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Produto/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Produto produto)
+        {
+            try
+            {
+                listaDeProdutos.Add(produto);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Produto/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Produto/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Produto/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Produto/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
